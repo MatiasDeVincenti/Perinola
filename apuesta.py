be60052424ec82d2d003080e7041c74619cabfa9
MatiasDeVincenti <mdevinceti@etec.uba.ar>
2024-09-02 09:43:29 -0300

    Creé la clase con el constructor y repr

---
+++ apuesta.py
@@ -0,0 +1,7 @@
+class Apuesta:
+    ##constructor 
+    def __init__(self):
+        self.fichas = 0
+    ##funcion para mostrar el print
+    def __repr__(self):
+        return f"Apuesta: {self.fichas} fichas" 
--- principal_objetos.py
+++ principal_objetos.py
@@ -1,5 +1,10 @@
 from perinola import Perinola
+from apuesta import Apuesta
 
-perinola = Perinola()
-perinola.tirar()
-print(perinola)
+# perinola = Perinola()
+# perinola.tirar()
+# print(perinola)
+
+ap = Apuesta()
+print(ap)
+
